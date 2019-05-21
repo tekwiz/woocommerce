2c82a2956f06cbe6754bbb54f8fb86ace1066910
Travis Warlick <twarlick@maxmedia.com>
2019-05-21 16:07:10 -0400

    Allow WC REST authentication to work for all WP REST API

--- includes/api/class-wc-rest-authentication.php
+++ includes/api/class-wc-rest-authentication.php
@@ -57,13 +57,16 @@ class WC_REST_Authentication {
 		$rest_prefix = trailingslashit( rest_get_url_prefix() );
 		$request_uri = esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) );
 
+		// Check if top-level REST request
+		$mainrest = ( false !== strpos( $request_uri, $rest_prefix ) );
+
 		// Check if the request is to the WC API endpoints.
 		$woocommerce = ( false !== strpos( $request_uri, $rest_prefix . 'wc/' ) );
 
 		// Allow third party plugins use our authentication methods.
 		$third_party = ( false !== strpos( $request_uri, $rest_prefix . 'wc-' ) );
 
-		return apply_filters( 'woocommerce_rest_is_request_to_rest_api', $woocommerce || $third_party );
+		return apply_filters( 'woocommerce_rest_is_request_to_rest_api', $woocommerce || $third_party || $mainrest );
 	}
 
 	/**
